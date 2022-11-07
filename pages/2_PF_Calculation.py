69fe41fdd873677bc8c7db5037ed9fc56d80fd13
ahembal <ahmbal22@student.hh.se>
2022-11-07 22:04:23 +0100

    add id key

--- pages/2_PF_Calculation.py
+++ pages/2_PF_Calculation.py
@@ -54,7 +54,10 @@ if 'input_variables' not in st.session_state: st.session_state['input_variables'
 
 }
 if 'plot_returns' not in st.session_state: st.session_state['plot_returns'] = {}
-if 'token' not in st.session_state: st.session_state['token'] = { 'timestamp':datetime.datetime.now(), 'token': None}
+if 'token' not in st.session_state: st.session_state['token'] = {
+    'timestamp':datetime.datetime.now(),
+    'token': None,
+    'id': None}
 
 def reload_def_params():
     st.session_state['input_variables'] = {
